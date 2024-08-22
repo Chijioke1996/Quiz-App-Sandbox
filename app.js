328d1e5ff4209bb99b5de01402096f3413f49eaf
unknown <ewuziejoke@gmail.com>
2024-08-22 19:41:08 +0100

    third message

--- app.js
+++ app.js
@@ -1,78 +1,63 @@
-const Questions = [
-    {
-        topic: 'tech',
-        question: 'What is the colour of the sky?',
-        possibleAnswers: ["Green", "Blue", "Red", "white"],
-        correctAnswer: "Blue"
-    },
+fetch("question.json")
+    .then((response) => response.json())
+    .then((Questions) => {
+        const progressBar = document.querySelector(".progress-bar")
+        const questionContainer = document.querySelector(".question-container")
+        const answerContainer = document.querySelector(".answer-container")
 
-    {
-        topic: 'science',
-        question: 'What branch of science studies life?',
-        possibleAnswers: ["Biology", "Agriculture", "Engineering"],
-        correctAnswer: 'Biology'
-    },
+        let currentQuestionIndex = 0
 
-    {
-        topic: 'Aviation',
-        question: 'Another name for a spin roll is what?',
-        possibleAnswers: ["Barrel roll", "Easy glide", "Bumper manoeuvre"],
-        correctAnswer: 'Barrel roll'
-    }
-]
+        //Quiz Handler 
+        function Quiz(index) {
 
-const progressBar = document.querySelector(".progress-bar")
-const questionContainer = document.querySelector(".question-container")
-const answerContainer = document.querySelector(".answer-container")
+            let correctchoice = 0
+            let wrongchoice = 0
 
-let currentQuestionIndex = 0
+            //Quiz progress
+            progressBar.innerHTML = ""
+            Questions.forEach((question) => {
+                progressBar.innerHTML += `<span class="span"></span>`
+            })
 
-//Quiz Handler 
-function Quiz(index) {
+            let spans = document.querySelectorAll(".span")
+            for (let i = 0; i <= index; i++) {
+                spans[i].classList.add("seen")
+            }
 
-    //Quiz progress
-    progressBar.innerHTML = ""
-    Questions.forEach((question) => {
-        progressBar.innerHTML += `<span class="span"></span>`
-    })
-
-    let spans = document.querySelectorAll(".span")
-    for (let i = 0; i <= index; i++) {
-        spans[i].classList.add("seen")
-    }
-
-    // topic/question
-    questionContainer.innerHTML = ""
-    questionContainer.innerHTML += `<p class="topic">${Questions[index].topic}</p>
+            // topic/question
+            questionContainer.innerHTML = ""
+            questionContainer.innerHTML += `<p class="topic">${Questions[index].topic}</p>
     <p class="question">${Questions[index].question}</p>`
 
-    //answer
-    answerContainer.innerHTML = ""
-    Questions[index].possibleAnswers.forEach((answer) => {
-        answerContainer.innerHTML += `<button>${answer}</button>`
+            //answer
+            answerContainer.innerHTML = ""
+            Questions[index].possibleAnswers.forEach((answer) => {
+                answerContainer.innerHTML += `<button>${answer}</button>`
+            })
+
+            const options = document.querySelectorAll("button")
+            options.forEach((option) => {
+                option.addEventListener("click", (e) => {
+                    if (e.target.textContent === Questions[index].correctAnswer) {
+                        alert("correct Answer")
+                        console.log("Correct");
+
+                    } else {
+                        alert("Wrong Answer")
+                        console.log("Wrong");
+
+                    }
+                    if (currentQuestionIndex === Questions.length - 1) {
+                        currentQuestionIndex = 0
+                    } else {
+                        currentQuestionIndex++
+                    }
+
+                    Quiz(currentQuestionIndex)
+                })
+            })
+        }
+
+        Quiz(currentQuestionIndex)
     })
 
-    const options = document.querySelectorAll("button")
-    options.forEach((option) => {
-        option.addEventListener("click", (e) => {
-            if (e.target.textContent === Questions[index].correctAnswer) {
-                alert("correct Answer")
-                console.log("Correct");
-
-            } else {
-                alert("Wrong Answer")
-                console.log("Wrong");
-                
-            }
-            if (currentQuestionIndex === Questions.length - 1) {
-                currentQuestionIndex = 0
-            } else {
-                currentQuestionIndex++
-            }
-
-            Quiz(currentQuestionIndex)
-        })
-    })
-}
-
-Quiz(currentQuestionIndex)
